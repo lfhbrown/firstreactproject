442651618ad4a24d574bbc962ad5074831610adc
6Weapons <89061083+lfhbrown@users.noreply.github.com>
2022-09-15 18:04:24 -0700

    Test

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,22 @@
+
+function ExpenseDate(props) {
+    
+    // toLocalString returns a string with a language-sensitive and human readable date
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    //getFullYear returns a 4 digit number. Is compliant with years after 2000
+    const year = props.date.getFullYear();
+
+    return (
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+    )
+};
+/*This allows us to export the ExpenseDate component (which is simply a function) so that it can be imported it into the ExpenseItem.js file. Remember this component logic was
+extracted from our ExpenseItem component because we want to keep our logic within each component as lean and narrow focused as possible. If a component can be split into multiple ocmponents, then it is
+good practice to do so*/
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,21 @@
+//This import allows us to use the styling from ExpenseItem.css. 
 import './ExpenseItem.css';
-
-function ExpenseItem() {
-
-    const expenseDate = new Date(2022, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+/*This is another component that originally was part of this ExpenseItem.js but to keep the component lean, the logic for our date object was split into it's own component 
+and called in on line 10 as opposed to having all the logic crowd out multiple lines in this component.*/
+import ExpenseDate from './ExpenseDate';
+/*This is a custom component (line 9-18). In the function parameters is 'props' which stands for property. Propsallows us to access the array of object key value pairs in 
+App.js (line 32-51).*/
+function ExpenseItem(props) {
+    //In the return statement is our JSX syntax. JSX is a combination of JavaScript   
     return (
     <div className="expense-item">
-        <div> {expenseDate.toISOString()}</div>
+        <ExpenseDate date= {props.date} ></ExpenseDate>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
     );
 };
-
+//This allows us to export the ExpenseItem component (which is simply a function) so that it can imported it into the App.js file.
 export default ExpenseItem;
